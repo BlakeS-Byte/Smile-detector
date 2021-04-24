6fd1e12990099999d3ece06d45284460dbea6ec0
Aren Aguas <arenjustinaguas@gmail.com>
2021-04-23 18:24:06 -0700

    Commented in a plot to show first 9 images of X_train, along with the y_train's categorical values as labels.

--- smilepatrol.py
+++ smilepatrol.py
@@ -3,13 +3,16 @@
 # 0->not happy
 # 1-> happy
 
-import numpy as np 
-import pandas as pd                         #data processing of csv files
-from sklearn.preprocessing import StandardScaler
-import keras                                #for cnn
-from tensorflow.python.keras.utils.np_utils import to_categorical    #I spent way to long trying to get this thing to work so here is this rediculous import for the categorical data
-import tensorflow as tf                     #for image data
+import numpy as np
 from numpy import genfromtxt
+import pandas as pd                         #data processing of csv files
+import keras                                #for cnn
+import tensorflow as tf                     #for image data
+from tensorflow.python.keras.utils.np_utils import to_categorical    #I spent way to long trying to get this thing to work so here is this rediculous import for the categorical data
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt             #plotting data as image
+from sklearn.preprocessing import StandardScaler
+
 
 test_data = pd.read_csv('test.csv')
 training_data = pd.read_csv('train.csv')
@@ -25,7 +28,7 @@ y_test = test_data.iloc[:,0:1]/255
 X_train = training_data.iloc[:,1:]/255
 X_test = test_data.iloc[:,1:]/255
 
-print(y_test)
+#print(y_test)
 
 #transfer data to a numpy array
 y_train = y_train.to_numpy()
@@ -41,6 +44,16 @@ X_test = X_test.reshape(X_test.shape[0],48,48,1)
 y_train = to_categorical(y_train,num_classes=2,dtype = "int32")
 y_test = to_categorical(y_test,num_classes=2, dtype= "int32")
 
+#Plots the first 9 images in X_train
+#plt.figure(figsize=(5,5))
+#for i in range(9):
+#    plt.subplot(3,3,i+1)
+#    plt.xticks([])
+#    plt.yticks([])
+#    plt.grid(False)
+#    plt.imshow(X_train[i], cmap=plt.cm.binary)
+#    plt.xlabel(y_train[i])
+#plt.show()
 
 #creating the convultional base CNN with tensor flow library takes in (image_height,image_width,color_channels)
 model = models.Sequential()
