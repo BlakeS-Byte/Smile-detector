f4718acd5e2814a161391064ea23e755602fc147
Aren Aguas <arenjustinaguas@gmail.com>
2021-04-25 17:14:15 -0700

    Fixed some formatting

--- Image processing/facedetect.py
+++ Image processing/facedetect.py
@@ -18,9 +18,8 @@ faces = detector.detectMultiScale(gray, 1.1, 4)
 
 #Draw rectangle around face and crop face
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), ( x +w, y+ h), (0, 0, 255), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
     faces = img[y:y + h, x:x + w]
-    #cv2.imshow("face", faces)
     cv2.imwrite('face.png', faces)
 
 #Saves copy of original image with detection box around face
@@ -28,6 +27,6 @@ cv2.imwrite('detection.jpg', img)
 
 #Convert cropped image to grayscale and scale down to 48x48 pixels
 scaling = Image.open('face.png').convert('LA')
-scaled_face = scaling.resize((48,48))
+scaled_face = scaling.resize((48, 48))
 scaled_face.save('face.png', 'PNG', optimize=True)
 
