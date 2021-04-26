3dbe16427538540c221192c9427c8ea10f03637a
aweston4 <78121492+aweston4@users.noreply.github.com>
2021-04-25 23:44:53 -0700

    Update facedetect.py
    updated to work with all custom images in the folder

--- Image processing/facedetect.py
+++ Image processing/facedetect.py
@@ -1,32 +1,39 @@
 #Detect and crop face from the designated image, then
 #convert it to greyscale and rescale down to 48x48 pixels
 import cv2
+import glob
+import shutil
+import os
 from PIL import Image
 
+direct = 'custom/formatted/'
+for f in os.listdir(direct):
+    os.remove(os.path.join(direct, f))
 
-#Read the input image
-img = cv2.imread('20210425_164112.jpg')
+for filename in glob.glob('custom/*.jpg'):
+    shutil.copy(filename, 'custom/formatted/')
 
-#Convert image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+for filename in glob.glob('custom/formatted/*.jpg'):
 
-#Load haar cascade
-detector = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    #Read the input image
+    img = cv2.imread(filename)
 
-#Detect faces
-faces = detector.detectMultiScale(gray, 1.1, 4)
+    #Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#Draw rectangle around face and crop face
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    faces = img[y:y + h, x:x + w]
-    cv2.imwrite('face.png', faces)
+    #Load haar cascade
+    detector = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 
-#Saves copy of original image with detection box around face
-cv2.imwrite('detection.jpg', img)
+    #Detect faces
+    faces = detector.detectMultiScale(gray, 1.1, 4)
 
-#Convert cropped image to grayscale and scale down to 48x48 pixels
-scaling = Image.open('face.png').convert('LA')
-scaled_face = scaling.resize((48, 48))
-scaled_face.save('face.png', 'PNG', optimize=True)
+    #Draw rectangle around face and crop face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        faces = img[y:y + h, x:x + w]
+        cv2.imwrite(filename, faces)
 
+    #Convert cropped image to grayscale and scale down to 48x48 pixels
+    scaling = Image.open(filename).convert('LA')
+    scaled_face = scaling.resize((48, 48))
+    scaled_face.save(filename, 'PNG', optimize=True)
