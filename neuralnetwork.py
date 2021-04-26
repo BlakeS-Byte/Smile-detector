2815122b390f54f2d87738ebad3d63269b3b0708
aweston4 <78121492+aweston4@users.noreply.github.com>
2021-04-26 01:25:49 -0700

    Update neuralnetwork.py
    updated to print out the custom photo predictions instead of 5 random smiles photos. Only catch is it prints out the formatted versions.

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -6,17 +6,20 @@ from PIL import Image
 
 # function prints out the first 5 images that are predicted to be smiles and are actually smiles
 def print_pred(i):
-    imt = X_test[i]
+    imt = X_custom[i]
     image2 = Image.fromarray(np.reshape(imt, [48, 48]))
     image2.show()
 
 dataset_train = loadtxt('train.csv', delimiter=',')            # take the training from the csv file
 dataset_test = loadtxt('test.csv', delimiter=',')              # take the test data
+dataset_customdata = loadtxt('customdata.csv', delimiter=',')  # take the custom data
 
 X_train = dataset_train[:, 1:]                                 # the training data
 X_test = dataset_test[:, 1:]                                   # the test data
 y_train = dataset_train[:, 0:1]                                # labels for the train data
 y_test = dataset_test[:, 0:1]                                  # labels for the test data
+X_custom = dataset_customdata[:, 1:]                           # the custom data
+y_custom = dataset_customdata[:, 0:1]                          # labels for the custom data
 
 model = Sequential()
 model.add(Dense(2500, input_dim=2304, activation='relu'))      # first layer
@@ -28,16 +31,18 @@ model.add(Dense(1, activation='sigmoid'))                      # fifth layer
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])   # combine the layers into full model
 model.fit(X_train, y_train, epochs=15, batch_size=1000)                             # fit the training data to the model
 
-model.summary()
+model.summary()                                                # prints summary of the neural network
 
 predictions = (model.predict(X_test) > 0.5).astype("int32")    # using the test data, predicts the labels
 accuracy = sum(np.array(predictions == y_test)) / len(X_test)  # checks the accuracy of the predictions
 print(accuracy * 100)                                          # prints out the accuracy
 
-count = 0                                                      # used to print a max of 5 images
-for i in range(len(X_test)):                                   # loops until 5 have been printed
-    if count == 5:                                             # breaks at 5
-        break
-    if predictions[i] == 1 and y_test[i] == 1:                 # checks for predicted true and actual true
-        print_pred(i)
-        count += 1
+predictions = (model.predict(X_custom) > 0.5).astype("int32")  # using the test data, predicts the labels of custom
+for i in range(len(X_custom)):                                 # print out the predictions for our custom images
+    print('Prediction for custom photo', i+1, 'is:', end='')
+    if predictions[i] == 0:
+        print(' No smile')
+    else:
+        print(' Smile')
+    print_pred(i)
+    input()
